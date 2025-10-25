595c1296e30b677fd1d3874f3a38b536116a2244
Poovarasan99830 <kingpoovarasan49@gmail.com>
2025-10-25 16:33:15 +0530

    remove code

--- midapp/views.py
+++ midapp/views.py
@@ -6,8 +6,4 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
-def index(request):
-    print('Index called and Entered in Airport', request.GET)
-    return JsonResponse({"message": "hi hello  welcome  in airport...!"})
 
